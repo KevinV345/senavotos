33aa2fc2c985ebd96aee10099fd1f2ce7c77b466
estefany2007-art <su_correo@github.com>
2025-09-17 07:46:08 -0500

    sdfdsf

--- src/app.py
+++ src/app.py
@@ -36,7 +36,7 @@ def base64_encode(data):
 # Configuración de la base de datos
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'DdiegoCenT4821#'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'senavotos'
 # Configuración de la carpeta para cargar archivos
 app.config['UPLOAD_FOLDER'] = 'uploads/'
